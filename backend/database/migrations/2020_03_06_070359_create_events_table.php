cf69d7020af1cfce3c9e5bbf523830e32e36aa3b
Ayush Mukherjee <ayushm123460@gmail.com>
2020-03-06 12:39:35 +0530

    init backend

---
+++ backend/database/migrations/2020_03_06_070359_create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->bigInteger('type')->unsigned();
+            $table->string('numberParticipants');
+            $table->timestamps();
+            $table->foreign('type')->references('id')->on('events_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
